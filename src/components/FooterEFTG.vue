9cd1c669d30afc8ae04d40589c1e3a2c45ce48c4
Julian Gonzalez <joticajulian@gmail.com>
2019-10-01 13:50:03 +0200

    footer version branch

--- src/components/FooterEFTG.vue
+++ src/components/FooterEFTG.vue
@@ -2,7 +2,7 @@
   <footer class="footer bg-primary text-light" style="min-height:2.5rem;">
     <div class="container-fluid">
       <div class="row pt-2 pb-2">
-        <div class="col-6 text-left"><small><a href="https://github.com/pablomat/pulsar-ui/" target="_blank"><img src="../assets/logo-github.png" style="height: 20px;" class="mr-1 align-bottom"/></a>Pulsar version {{version}}</small></div>
+        <div class="col-6 text-left"><small><a href="https://github.com/pablomat/pulsar-ui/" target="_blank"><img src="../assets/logo-github.png" style="height: 20px;" class="mr-1 align-bottom"/></a>Pulsar-UI — EFTG — version {{version}}</small></div>
       </div>
     </div>
   </footer>
